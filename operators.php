3e55b6b4c633c07186ee684ef66fca0f1cb43458
Andres Auais <andresauais@gmail.com>
2021-01-29 10:58:56 +0000

    dates.php

--- operators.php
+++ operators.php
@@ -1,2 +1,18 @@
 <?php
-var_dump(1==2);
+var_dump(1+2); echo "(1+2) <br>";
+var_dump(1-2); echo "(1-2) <br>";
+var_dump(1*2); echo "(1*2) <br>";
+var_dump(1/2); echo "(1/2) <br>";
+var_dump(1%2); echo "(1%2) <br>";
+
+var_dump(1==2); echo "(1==2) <br>";
+var_dump(1!=2); echo "(1!=2) <br>";
+var_dump(1<2); echo "(1<2) <br>";
+var_dump(1>2); echo "(1>2) <br>";
+var_dump(1<=2); echo "(1<=2) <br>";
+var_dump(1>=2); echo "(1>=2) <br>";
+
+var_dump(true&&false); echo "(true&&false) <br>";
+var_dump(true||false); echo "(true&||false) <br>";
+var_dump(true!=false); echo "(true!false) <br>";
+
