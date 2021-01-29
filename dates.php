874c4efad26cf767ffa692fbb462517344d7b516
Andres Auais <andresauais@gmail.com>
2021-01-29 11:05:19 +0000

    conditionals.php

--- dates.php
+++ dates.php
@@ -1,1 +1,7 @@
 <?php
+
+echo "Today is ". date("Y/m/d") . "<br>";
+echo "Today is " . date("Y.m.d") . "<br>";
+echo "Today is " . date("l") . "<br>";
+echo "Today is " . date("m") . "<br>";
+echo "The current mins are " . date("i") . "<br>";
