c2bcd812b70024cfc420b4b7c7b3bc896cc3e364
Andres Auais <andresauais@gmail.com>
2021-01-29 16:20:07 +0000

    arrays.php

--- arrays.php
+++ arrays.php
@@ -1,1 +1,14 @@
 <?php
+$sArray = ["Hello", "php", "!"];
+echo "$sArray <br>";
+$nArray = [1, 2, 3, 4, 5.5, 6.2, 7.3];
+echo "$nArray <br>";
+$mArray = [$sArray, $nArray];
+echo "$mArray <br>";
+
+echo count($sArray) . "<br>";
+echo print_r(array_merge($sArray, $nArray)) . "<br>";
+echo end($sArray) . "<br>";
+
+array_push($nArray, 89);
+print_r($nArray);
