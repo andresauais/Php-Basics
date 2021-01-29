378ba6d49f38e8a8461bca0c69e17ce5a68a4871
Andres Auais <andresauais@gmail.com>
2021-01-29 10:42:33 +0000

    operators.php

--- iterators.php
+++ iterators.php
@@ -1,4 +1,21 @@
 <?php
-for($i = 0; $x < 5; $x++){
-  echo "This is a for loop $x";
+for($i = 0; $i < 5; $i++){
+  echo "This is a for loop $i <br>";
 }
+
+$array = [1, 2, 3, 4, 5];
+foreach($array as $value){
+  echo "foreach $value <br>";
+}
+
+$x = 5;
+while($x > 0){
+  echo "While loop $x <br>";
+  $x--;
+}
+
+$y = 0;
+do{
+  echo "Do while loop $y <br>";
+  $y++;
+} while($y <5);
